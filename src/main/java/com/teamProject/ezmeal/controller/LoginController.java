df24758da0cdc8d535a015e2670b10b8fae2cc1a
hubaek <zjfl3122@naver.com>
2023-06-23 20:14:34 +0900

    Feat : login

--- src/main/java/com/teamProject/ezmeal/controller/LoginController.java
+++ src/main/java/com/teamProject/ezmeal/controller/LoginController.java
@@ -1,30 +1,29 @@
 package com.teamProject.ezmeal.controller;
 
-import java.net.URLEncoder;
+import com.teamProject.ezmeal.dao.MemberDao;
+import com.teamProject.ezmeal.domain.MemberDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import com.teamProject.ezmeal.dao.*;
-import com.teamProject.ezmeal.domain.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.lang.reflect.Member;
+import java.net.URLEncoder;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
     @Autowired
-    UserDao userDao;
+    MemberDao memberDao;
 
     @GetMapping("/login")
-    public String loginForm() {
-        return "loginForm";
+    public String login() {
+        return "login";
     }
 
     @GetMapping("/logout")
@@ -36,33 +35,33 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(String id, String pwd, String toURL, boolean rememberId,
+    public String login(String lgin_id, String lgin_pw, String toURL, boolean rememberId,
                         HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        System.out.println("id = " + id);
-        System.out.println("pwd = " + pwd);
+        System.out.println("id = " + lgin_id);
+        System.out.println("pwd = " + lgin_pw);
 
-        // 1. id와 pwd를 확인
-        if (!loginCheck(id, pwd)) {
+        // 1. lgin_id와 lgin_pw를 확인
+        if (!loginCheck(lgin_id, lgin_pw)) {
             // 2-1   일치하지 않으면, loginForm으로 이동
             String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.", "utf-8");
 
-            return "redirect:/login/login?msg=" + msg;
+            return "redirect:/login?msg=" + msg;
         }
-        // 2-2. id와 pwd가 일치하면,
+        // 2-2. lgin_id와 lgin_pw가 일치하면,
         //  세션 객체를 얻어오기
         HttpSession session = request.getSession();
-        //  세션 객체에 id를 저장
-        session.setAttribute("id", id);
+        //  세션 객체에 lgin_id를 저장
+        session.setAttribute("lgin_id", lgin_id);
 
         if (rememberId) {
             //     1. 쿠키를 생성
-            Cookie cookie = new Cookie("id", id); // ctrl+shift+o 자동 import
+            Cookie cookie = new Cookie("lgin_id", lgin_id); // ctrl+shift+o 자동 import
 //		       2. 응답에 저장
             response.addCookie(cookie);
         } else {
             // 1. 쿠키를 삭제
-            Cookie cookie = new Cookie("id", id); // ctrl+shift+o 자동 import
+            Cookie cookie = new Cookie("lgin_id", lgin_id); // ctrl+shift+o 자동 import
             cookie.setMaxAge(0); // 쿠키를 삭제
 //		       2. 응답에 저장
             response.addCookie(cookie);
@@ -73,17 +72,17 @@ public class LoginController {
         return "redirect:" + toURL;
     }
 
-    private boolean loginCheck(String id, String pwd) {
-        User user = null;
+    private boolean loginCheck(String lgin_id, String lgin_pw) {
+        MemberDto memberDto = null;
 
         try {
-            user = userDao.selectUser(id);
+            memberDto = memberDao.selectMember(lgin_id);
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
 
-        return user != null && user.getPwd().equals(pwd);
-//        return "asdf".equals(id) && "1234".equals(pwd);
+        return memberDto != null && memberDto.getLgin_pw().equals(lgin_pw);
+//        return "asdf".equals(lgin_id) && "1234".equals(pwd);
     }
 }
