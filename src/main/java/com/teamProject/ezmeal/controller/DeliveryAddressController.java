0c61c7654ac1543c5cce8551da3250cc4c24cf31
taewan625 <taewan1997625@gmail.com>
2023-06-26 17:29:33 +0900

    git switch 문제로 일시적 commit -> 나중에 commit 합치기 필요2

--- src/main/java/com/teamProject/ezmeal/controller/DeliveryAddressController.java
+++ src/main/java/com/teamProject/ezmeal/controller/DeliveryAddressController.java
@@ -1,16 +1,28 @@
 package com.teamProject.ezmeal.controller;
 
+import com.teamProject.ezmeal.dao.DeliveryAddressDao;
+import com.teamProject.ezmeal.domain.DeliveryAddressDto;
+import com.teamProject.ezmeal.service.DeliveryAddressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/address")
-public class AddressController {
+public class DeliveryAddressController {
+    private final DeliveryAddressService deliveryAddressService;
+    private final DeliveryAddressDao deliveryAddressDao;
+
     @GetMapping
-    public String addressList(){
+    public String addressList(@SessionAttribute() Model model) throws Exception {
+        List<DeliveryAddressDto> addressList = deliveryAddressDao.addressList(10001);
+        model.addAttribute("addressList",addressList);
         return "addressList";
     }
 }
--- src/main/java/com/teamProject/ezmeal/service/DeliveryAddressService.java
+++ src/main/java/com/teamProject/ezmeal/service/DeliveryAddressService.java
@@ -1,2 +1,13 @@
-package com.teamProject.ezmeal.service;public class DeliveryAddressService {
+package com.teamProject.ezmeal.service;
+
+import com.teamProject.ezmeal.dao.DeliveryAddressDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryAddressService {
+    private final DeliveryAddressDao deliveryAddressDao;
+
+
 }
