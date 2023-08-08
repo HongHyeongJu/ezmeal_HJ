04a3e2c116d91a4db997d8a9c3d77ae973b4968b
imk <dlwldmsgo808@gmail.com>
2023-08-08 18:13:43 +0900

    (0808)feat:notice dashboard 수정

--- src/main/java/com/teamProject/ezmeal/controller/AdminNoticeController.java
+++ src/main/java/com/teamProject/ezmeal/controller/AdminNoticeController.java
@@ -1,8 +1,11 @@
 package com.teamProject.ezmeal.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.sql.SQLException;
 
 @Controller
 @RequestMapping("/admin/notice")
@@ -11,5 +14,9 @@ public class AdminNoticeController {
     public String adminNoticeWrite(){
       return  "admin_notice_write";
     }
-}
 
+
+    @GetMapping("/dashboard")
+    public String adminboardeHome(Model model)  {return "admin_board_notice";}
+
+}
